38d8f978294f85b65d16d23d629fe4156094dcde
rayms <rayms.dev@gmail.com>
2018-11-21 15:49:39 +0100

    #2 Rename root component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Hello } from './components/Hello';
+import { App } from './components/App';
 // tslint:disable-next-line:no-any
 declare let module: any;
 
-ReactDOM.render(<Hello compiler="Typescript" framework="React" bundler="Webpack" />,
+ReactDOM.render(<App compiler="Typescript" framework="React" bundler="Webpack" />,
     document.getElementById('root'));
 
 if (module.hot) {
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path'),
 module.exports = {
     mode: 'development',
     entry: {
-        app: ['./src/app/App.tsx', 'webpack-hot-middleware/client'],
+        app: ['./src/app/index.tsx', 'webpack-hot-middleware/client'],
         vendor: ['react', 'react-dom']
     },
     output: {
