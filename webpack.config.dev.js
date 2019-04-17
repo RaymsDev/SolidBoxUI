ba3570f4a00734e3368cbf68d28a5d961a81ea91
rayms <rayms.dev@gmail.com>
2019-04-17 15:20:30 +0200

    Add favicon

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,17 +2,18 @@ const path = require('path'),
   webpack = require('webpack'),
   HtmlWebpackPlugin = require('html-webpack-plugin');
 const sourcePath = path.resolve(__dirname, 'src');
+
 const port = 3000;
 module.exports = {
   context: sourcePath,
   mode: 'development',
   entry: {
     app: ['./app/App.tsx'],
-    vendor: ['react', 'react-dom']
+    vendor: ['react', 'react-dom'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app/[name].bundle.js'
+    filename: 'app/[name].bundle.js',
   },
   devtool: 'source-map',
 
@@ -20,17 +21,17 @@ module.exports = {
     contentBase: sourcePath,
     hot: true,
     port: port,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   node: {
     // workaround for webpack-dev-server issue
     // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
     fs: 'empty',
-    net: 'empty'
+    net: 'empty',
   },
 
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
   module: {
     rules: [
@@ -38,7 +39,7 @@ module.exports = {
         test: /\.scss$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: 'style-loader',
           },
           {
             loader: 'typings-for-css-modules-loader',
@@ -47,31 +48,31 @@ module.exports = {
               modules: true,
               namedExport: true,
               sourceMap: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            }
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
           },
           {
-            loader: 'sass-loader'
-          }
-        ]
+            loader: 'sass-loader',
+          },
+        ],
       },
       {
         test: /\.(ts|tsx)$/,
         enforce: 'pre',
         use: [
           {
-            loader: 'tslint-loader'
-          }
-        ]
+            loader: 'tslint-loader',
+          },
+        ],
       },
       {
         test: /\.(ts|tsx)$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'awesome-typescript-loader',
       },
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
@@ -80,26 +81,26 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
+              outputPath: 'fonts/',
+            },
+          },
+        ],
       },
       {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
             loader: 'file-loader',
-            options: {}
-          }
-        ]
-      }
-    ]
+            options: {},
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html')
+      template: path.resolve(__dirname, 'src', 'index.html'),
     }),
-    new webpack.WatchIgnorePlugin([/scss\.d\.ts$/])
-  ]
+    new webpack.WatchIgnorePlugin([/scss\.d\.ts$/]),
+  ],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,43 @@ const path = require('path'),
   webpack = require('webpack'),
   HtmlWebpackPlugin = require('html-webpack-plugin');
 const sourcePath = path.join(__dirname, './src');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 module.exports = {
   context: sourcePath,
   mode: 'production',
   entry: {
-    app: ['./app/App.tsx',],
-    vendor: ['react', 'react-dom']
+    app: ['./app/App.tsx'],
+    vendor: ['react', 'react-dom'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app/[name].[chunkhash].bundle.js'
+    filename: 'app/[name].[chunkhash].bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-
           {
-            loader: "style-loader"
+            loader: 'style-loader',
           },
           {
-            loader: "typings-for-css-modules-loader",
+            loader: 'typings-for-css-modules-loader',
             options: {
               camelcase: true,
               modules: true,
               namedExport: true,
               sourceMap: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            }
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
           },
           {
-            loader: "sass-loader",
-          }]
+            loader: 'sass-loader',
+          },
+        ],
       },
       {
         test: /\.(ts|tsx)$/,
@@ -45,12 +46,12 @@ module.exports = {
         use: [
           {
             loader: 'tslint-loader',
-          }
-        ]
+          },
+        ],
       },
       {
         test: /\.(ts|tsx)$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'awesome-typescript-loader',
       },
       {
         test: /\.css$/,
@@ -58,13 +59,15 @@ module.exports = {
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
+        use: [
+          {
             loader: 'file-loader',
             options: {
-                name: '[name].[ext]',
-                outputPath: 'fonts/'
-            }
-        }]
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
       },
       {
         test: /\.(png|jpg|gif)$/,
@@ -75,12 +78,46 @@ module.exports = {
           },
         ],
       },
-    ]
+    ],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
-    new webpack.WatchIgnorePlugin([
-      /scss\.d\.ts$/
-    ]),
-  ]
-}
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new webpack.WatchIgnorePlugin([/scss\.d\.ts$/]),
+    new FaviconsWebpackPlugin({
+      // Your source logo
+      logo: path.resolve(
+        __dirname,
+        'src',
+        'assets',
+        'logo',
+        'logo_teamwork.png',
+      ),
+      // The prefix for all image files (might be a folder or a name)
+      prefix: 'favicon-[hash]/',
+      // Emit all stats of the generated icons
+      // Generate a cache file with control hashes and
+      // don't rebuild the favicons until those hashes change
+      persistentCache: true,
+      // Inject the html into the html-webpack-plugin
+      inject: true,
+      // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+      title: 'TeamWork',
+
+      // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false,
+      },
+    }),
+  ],
+};
